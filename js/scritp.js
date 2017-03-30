bfb63395a0059aa6c3ee257900c40e42f50dfc45
aprietof <adrianprieto00@gmail.com>
2017-03-29 21:53:09 -0400

    fixed mobile bouning top nav bug

--- js/scritp.js
+++ js/scritp.js
@@ -1,19 +1,25 @@
 $(document).ready(function(){
     var lastScrollTop = 0;
 
-  /* On Scroll
-  ---------------------------------------------------*/
-  $(window).scroll(function(){
-      
-    var currenPosition = $(this).scrollTop();
-    var nav = $('#nav');
+    /* On Scroll
+    ---------------------------------------------------*/
+    $(window).scroll(function(){
 
-    if(currenPosition > lastScrollTop) {
-        nav.removeClass("headroom--pinned").addClass("headroom--unpinned");
-    }else if (currenPosition < 100 || currenPosition < lastScrollTop){
-        nav.removeClass("headroom--unpinned").addClass("headroom--pinned");
-    } 
-    lastScrollTop = currenPosition;
-  });
+        var currenPosition = $(this).scrollTop();
+        var nav = $('#nav');
+
+        if(currenPosition > lastScrollTop) {
+            nav.removeClass("headroom--pinned").addClass("headroom--unpinned");
+        }else{
+            nav.removeClass("headroom--unpinned").addClass("headroom--pinned");
+        }
+        
+        lastScrollTop = currenPosition;
+        
+        if(lastScrollTop < 100) {
+           nav.removeClass("headroom--unpinned").addClass("headroom--pinned"); 
+        }
+    
+    });
 
 });
