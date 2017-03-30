22c39217037b9c79effe875c59b9d70ef0a5f789
aprietof <adrianprieto00@gmail.com>
2017-03-29 21:13:56 -0400

    footer and nav show at scroll up added

--- js/scritp.js
+++ js/scritp.js
@@ -1,17 +1,19 @@
 $(document).ready(function(){
+    var lastScrollTop = 0;
 
   /* On Scroll
   ---------------------------------------------------*/
   $(window).scroll(function(){
+      
     var currenPosition = $(this).scrollTop();
     var nav = $('#nav');
-    var triggerPosition = 100;
 
-    if(currenPosition > triggerPosition) {
+    if(currenPosition > lastScrollTop) {
         nav.removeClass("headroom--pinned").addClass("headroom--unpinned");
     }else{
         nav.removeClass("headroom--unpinned").addClass("headroom--pinned");
     }
+      lastScrollTop = currenPosition;
   });
 
 });
