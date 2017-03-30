8c804defb60e37784927d676a58a79e225ff11b1
aprietof <adrianprieto00@gmail.com>
2017-03-29 22:34:34 -0400

    mobile nav bug fixed

--- js/scritp.js
+++ js/scritp.js
@@ -1,21 +1,39 @@
-$(document).ready(function(){
+// Hide Header on on scroll down
+    var didScroll;
     var lastScrollTop = 0;
+    var delta = 5;
+    var navbarHeight = $('#nav').outerHeight();
 
-    /* On Scroll
-    ---------------------------------------------------*/
-    $(window).scroll(function(){
-
-        var currenPosition = $(this).scrollTop();
-        var nav = $('#nav');
-
-        if(currenPosition > lastScrollTop) {
-            nav.removeClass("headroom--pinned").addClass("headroom--unpinned");
-        }else{
-            nav.removeClass("headroom--unpinned").addClass("headroom--pinned");
-        }
-        
-        lastScrollTop = currenPosition;
+function hasScrolled() {
+    var st = $(this).scrollTop();
     
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    if (st > lastScrollTop && st > navbarHeight){
+        $('#nav').removeClass("headroom--pinned").addClass("headroom--unpinned");
+    } else {
+        if(st + $(window).height() < $(document).height()) {
+            $('#nav').removeClass("headroom--unpinned").addClass("headroom--pinned");
+        }
+    }
+    
+    lastScrollTop = st;
+}
+
+
+$(document).ready(function(){
+    
+
+    $(window).scroll(function(event){
+        didScroll = true;
     });
 
-});
+    setInterval(function() {
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
+        }
+    }, 250);
+})
+
